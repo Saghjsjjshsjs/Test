75b931ea4c88071cc2c57ed38441f502a5f78941
Sahil <167304810+Saghjsjjshsjs@users.noreply.github.com>
2024-05-10 20:27:20 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+
+
+
+echo "Cloning Repo...."
+git clone https://github.com/Saghjsjjshsjs/Test  /MissPerfectURLBOT
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python main.py
